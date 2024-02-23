ea93b67d385ea03978b11f2eee509bb085b24c6d
Dorival Pedroso <dorival.pedroso@gmail.com>
2024-02-23 15:05:14 +1000

    Rename example files

---
+++ gen-reference-files.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+SOURCE=`pwd`
+cd /tmp
+mkdir -p build-hwode
+cd build-hwode/
+cmake -S $SOURCE
+make
+
+RUSSELL="/home/dorival/01-Code/rust/russell/russell_ode/data"
+
+cd examples
+
+./dop853_vdp        > "$RUSSELL/fortran_dop853_van_der_pol.txt"
+./dopri5_aren_debug > "$RUSSELL/fortran_dopri5_arenstorf_debug.txt"
+./dopri5_aren       > "$RUSSELL/fortran_dopri5_arenstorf.txt"
+./dopri5_eq1        > "$RUSSELL/fortran_dopri5_hairer_wanner_eq1.txt"
+./radau5_amp_debug  > "$RUSSELL/fortran_radau5_amplifier_debug.txt"
+./radau5_amp        > "$RUSSELL/fortran_radau5_amplifier.txt"
+./radau5_eq1_debug  > "$RUSSELL/fortran_radau5_hairer_wanner_eq1_debug.txt"
+./radau5_eq1        > "$RUSSELL/fortran_radau5_hairer_wanner_eq1.txt"
+./radau5_vdp_debug  > "$RUSSELL/fortran_radau5_van_der_pol_debug.txt"
+./radau5_vdp        > "$RUSSELL/fortran_radau5_van_der_pol.txt"
