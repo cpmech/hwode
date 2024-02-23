29d07828d21ffbd95226c98df3f0498dc8918b3b
Dorival Pedroso <dorival.pedroso@gmail.com>
2024-02-23 16:02:13 +1000

    Impl example dopri5 robertson

--- gen-reference-files.bash
+++ gen-reference-files.bash
@@ -15,6 +15,7 @@ cd examples
 ./dopri5_aren_debug > "$RUSSELL/fortran_dopri5_arenstorf_debug.txt"
 ./dopri5_aren       > "$RUSSELL/fortran_dopri5_arenstorf.txt"
 ./dopri5_eq1        > "$RUSSELL/fortran_dopri5_hairer_wanner_eq1.txt"
+./dopri5_rob        > "$RUSSELL/fortran_dopri5_robertson.txt"
 ./radau5_amp_debug  > "$RUSSELL/fortran_radau5_amplifier_debug.txt"
 ./radau5_amp        > "$RUSSELL/fortran_radau5_amplifier.txt"
 ./radau5_eq1_debug  > "$RUSSELL/fortran_radau5_hairer_wanner_eq1_debug.txt"
